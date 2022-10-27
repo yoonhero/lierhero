d23282445de40334bd9ff70f1307fe572be74cde
yoonhero <yoonhero06@naver.com>
2022-10-28 00:17:05 +0900

    feat: Collect Data Web GUI

---
+++ app.py
@@ -0,0 +1,92 @@
+from flask import (
+    Flask, redirect, render_template, request, session,jsonify, url_for, Response
+)
+import time
+from collect_data import DataSet, get_timestamp
+from utils import LineChart
+from webcam import Webcam
+import math
+import cv2
+import pandas as pd
+
+app = Flask(__name__)
+
+dataset = DataSet()
+
+WIDTH, HEIGHT = (680, 400)
+cam = Webcam(0, WIDTH, HEIGHT)
+
+
+
+rec_condition = False
+lie = False
+
+
+
+def generate_frames():
+    while True:
+        global rec_condition
+        global lie
+        global dataset
+            
+        ## read the camera frame
+        frame = cam.get_image()
+        if rec_condition:
+            ts = math.floor(get_timestamp())
+            
+            filepath = f"./dataset/lie/{ts}.jpg" if lie else f"./dataset/not_lie/{ts}.jpg"
+
+            cv2.imwrite(filepath, frame)
+
+            # TODO: Pandas LIST Save
+            new_row = pd.DataFrame({"img": filepath, "heart_rate": [1, 2, 3, 5, 6, 7, 8,9 , 10, 1], "lie": int(lie)})
+            dataset.add_row(new_row)
+
+            time.sleep(0.1)
+
+        ret,buffer=cv2.imencode('.jpg',frame)
+        frame=buffer.tobytes()
+
+
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/', methods=["GET"])
+def index():
+
+    return render_template('base.html')
+
+
+@app.route("/video")
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route("/lie/<condition>", methods=["GET"])
+def change_lie_condition(condition):
+    try:
+        global lie
+        lie = bool(int(condition))
+
+        return jsonify({"message": "Success!!"})
+    except:
+        return jsonify({"message": "Fail..."})
+
+@app.route("/rec/<condition>", methods=["GET"])
+def change_rec_condition(condition):
+    try: 
+        global rec_condition
+        rec_condition = bool(int(condition))
+
+        if not rec_condition:
+            dataset.save("./data.csv")
+
+        return jsonify({"message":"Success!!"})
+    except:
+        return jsonify({"message":"Fail..."})
+
+
+
+
+app.run(host = '127.0.0.1', port=8080)
--- collect_data.py
+++ collect_data.py
@@ -2,14 +2,13 @@ import pandas as pd
 import time
 from utils import LineChart
 from webcam import Webcam
-from arduino import ArduinoInput, get_usb_device
+# from arduino import ArduinoInput, get_usb_device
 from datetime import datetime
 import math
 import cv2
-import keyboard
-from pygame.locals import *
-from pynput import keyboard
-
+# import keyboard
+# from pygame.locals import *
+# from pynput import keyboard
 
 
 def get_timestamp():
@@ -29,86 +28,157 @@ class DataSet(object):
         self.dataframe = pd.read_csv(filepath)
 
     def add_row(self, row:dict):
-        self.dataframe = self.dataframe.append(row, ignore_index=True)
+        self.dataframe = pd.concat([self.dataframe, row])
 
     def show_data(self):
         return self.dataframe
 
-    def save(self):
-        self.dataframe.to_csv("dataset.csv", index=False)
+    def save(self, filename):
+        self.dataframe.to_csv(filename, index=False)
+
+
+class CollectDataTool():    
+    WIDTH, HEIGHT = (680, 400)
+    def __init__(self):
+        self.dataset = DataSet()
+        self.cam = Webcam(0, CollectDataTool.WIDTH, CollectDataTool.HEIGHT)
+        # self.sensor = ArduinoInput(get_usb_device()[-1])
+        ## TODO: Chart for Data visualizing
+        # self.chart = LineChart()
+
+        self.lie = False
+
+        self.rec_condition = False
+
+
+    def change_lie_condition(self, lie):
+        self.lie = lie
+
+    def process(self):
+        if not self.rec_condition:
+            print("NONO")
+            time.sleep(0.1)
+            return
+        img = self.cam.get_image()
+
+        sensor_values = []
+        for _ in range(10):
+            v = self.sensor.get_data()
+            sensor_values.append(int(v))
+            # self.chart.add_value(math.floor(get_timestamp()), v)
+            time.sleep(0.1)
+
+        ts = math.floor(get_timestamp())
+            
+        filepath = f"./dataset/lie/{ts}.jpg" if self.lie else f"./dataset/not_lie/{ts}.jpg"
+
+        cv2.imwrite(filepath, img)
+
+        new_row = pd.DataFrame({"img": filepath, "heart_rate": [1, 2, 3, 5, 6, 7, 8,9 , 10, 1], "lie": int(self.lie)})
+        self.dataset.add_row(new_row)
+
+        time.sleep(0.1)
+
+    def start(self):
+        self.rec_condition = True
+
+
+        # chart.visualize()
+    def end(self, save_file_name):
+        self.rec_condition = False
+        self.dataset.save(save_file_name)
+
+        self.cam.stop()
+
+        
+
 
 def on_press(key):
     if key == keyboard.Key.space:
         return True
     return False
+
+
+
     
 if __name__ == '__main__':
+    collect_data_tool = CollectDataTool()
+
+    collect_data_tool.start()
+
+    time.sleep(2)
+
+    print("Finish Recording")
+
+    collect_data_tool.end("result.csv")
     
 
-    dataset = DataSet()
-    # dataset.load("dataset.csv")
+    # dataset = DataSet()
+    # # dataset.load("dataset.csv")
 
-    device = 0
-    WIDTH = 640
-    HEIGHT = 400
-    cam = Webcam(device, WIDTH, HEIGHT)
+    # device = 0
+    # WIDTH = 640
+    # HEIGHT = 400
+    # cam = Webcam(device, WIDTH, HEIGHT)
 
-    sensor = ArduinoInput(get_usb_device()[-1])
+    # sensor = ArduinoInput(get_usb_device()[-1])
 
-    chart = LineChart()
+    # chart = LineChart()
 
-    lie = False
+    # lie = False
 
     
-    for i in range(3):
-        lie = False
+    # for i in range(3):
+    #     lie = False
 
-        # with keyboard.Events() as events:
-        #     for event in events:
-        #         if event.key == keyboard.Key.space:
-        #             lie = True
-        #             print("LIE")
-        #             break
-        #         else:
-        #             break
-        # key = cv2.waitKey()
+    #     # with keyboard.Events() as events:
+    #     #     for event in events:
+    #     #         if event.key == keyboard.Key.space:
+    #     #             lie = True
+    #     #             print("LIE")
+    #     #             break
+    #     #         else:
+    #     #             break
+    #     # key = cv2.waitKey()
 
-        # if key == ord('a'):
-        #     lie = True 
+    #     # if key == ord('a'):
+    #     #     lie = True 
 
 
-        img = cam.get_image()
+    #     img = cam.get_image()
 
-        # if key == keyboard.Key.esc:
-        #     print("LIE")
-        #     lie = True
+    #     # if key == keyboard.Key.esc:
+    #     #     print("LIE")
+    #     #     lie = True
 
-        sensor_values = []
-        for _ in range(10):
-            v = sensor.get_data()
-            sensor_values.append(int(v))
-            chart.add_value(math.floor(get_timestamp()), v)
-            time.sleep(0.1)
+    #     sensor_values = []
+    #     for _ in range(10):
+    #         v = sensor.get_data()
+    #         sensor_values.append(int(v))
+    #         chart.add_value(math.floor(get_timestamp()), v)
+    #         time.sleep(0.1)
 
-        ts = math.floor(get_timestamp())
+    #     ts = math.floor(get_timestamp())
 
-        print("\rts")
+    #     print("\rts")
         
-        filepath = f"./dataset/lie/{ts}.jpg" if lie else f"./dataset/not_lie/{ts}.jpg"
+    #     filepath = f"./dataset/lie/{ts}.jpg" if lie else f"./dataset/not_lie/{ts}.jpg"
 
-        cv2.imwrite(filepath, img)
+    #     cv2.imwrite(filepath, img)
 
-        new_row = pd.DataFrame({"img": filepath, "heart_rate": sensor_values, "lie": lie})
-        dataset.add_row(new_row)
+    #     new_row = pd.DataFrame({"img": filepath, "heart_rate": sensor_values, "lie": lie})
+    #     dataset.add_row(new_row)
 
-        # print(dataset.show_data)
+    #     # print(dataset.show_data)
 
-        time.sleep(0.1)
+    #     time.sleep(0.1)
     
 
-    chart.visualize()
+    # chart.visualize()
 
-    dataset.save()
+    # dataset.save()
 
-    cam.stop()
+    # cam.stop()
+
+
 
